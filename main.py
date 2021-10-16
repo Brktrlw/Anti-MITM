5eeb714496f80a8bbf97664221dec94499dd1635
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-16 21:55:21 +0300

    Update

--- main.py
+++ main.py
@@ -3,96 +3,92 @@ import os
 import time
 from pygame import mixer
 from datetime import datetime
-
 import scapy.all as scapy
+class AntiMITM():
+    def __init__(self):
+        self.REAL_GATEWAY_MAC=None
+        self.REAL_GATEWAY_IP=None
+        self.MAC_ADDRESS_LIST=list()
+        self.MAC_ADDRESS_LIST_FOR_DETECT_ATTACKER=list()
+        self.IP_ADDRESS_LIST=list()
+        self.TARGET_MAC_ADDRESS=None
+        self.TARGET_IP_ADDRESS=None
+        self.ZIP_IP_AND_MAC=None
 
-kayit = 0
-
-
-
-target_ip = None
-ip_address=list()
-mac_address=list()
-original_modem_mac=None
-original_modemIP=None
-time.sleep(2)
-with os.popen('arp -a') as f:
-    data2 = f.read()
-sayac=0
-macAddressList=list()
-for lines in data2.splitlines():
-    sayac+=1
-    if sayac==4:
-        original_modem_mac=lines.split()[1]
-        original_modemIP=lines.split()[0]
-while True:
-    time.sleep(2)
-    with os.popen('arp -a') as f:
-        data = f.read()
-
-    sayac=0
-    macAddressList=list()
-    for line in data.splitlines():
-        sayac+=1
-        if sayac==4:
-            modemMac=line.split()[1]
-            modemIP=line.split()[0]
-        else:
-            if line.split()==[]:
+    def setRealGatewayMac(self):                             # tek bir sefer calısır ve orijinal modem mac adresini ayarlar
+        with os.popen('arp -a') as f:
+            arpTable = f.read()
+        for lines in arpTable.splitlines():
+            if lines.split()==[] or "Interface:" in lines.split() or "Internet" in lines.split():
                 pass
             else:
-                if "Internet" in line.split() or "192.168.21.48" in line.split():
-                    pass
-                else:
-                    macAddressList.append(line.split()[1])
+                self.REAL_GATEWAY_MAC=lines.split()[1]
+                self.REAL_GATEWAY_IP=lines.split()[0]
+                break
 
-    #macAddressList.append(modemMac)
-    if modemMac in macAddressList or original_modem_mac!=modemMac:
-        mixer.init()
-        mixer.music.load('dangerSound.mp3')
-        mixer.music.play()
-        if kayit == 0:
-            with open("logs.txt","a") as file:
-                file.write(data)
-                file.write("TARİH= "+str(datetime.now()))
-                kayit=1
-
-        gateway_ip = modemIP
-
-        for dataline in data.splitlines():
-            if dataline.split()==[]:
+    def updateMacAddressList(self):
+        with os.popen('arp -a') as f:                                      # Mac adreslerin bulunduğu listeyi günceller
+            self.arpTable = f.read()
+        for lines in self.arpTable.splitlines():
+            if lines.split() == [] or "Interface:" in lines.split() or "Internet" in lines.split():
                 pass
             else:
-                if "Interface:" in dataline.split() or "Internet" in dataline.split():
-                    pass
-                else:
-                    ip_address.append(dataline.split()[0])
-                    mac_address.append(dataline.split()[1])
-        arp_response = scapy.ARP(op=2, pdst=target_ip, hwdst=original_modem_mac, psrc="192.168.20.1")
-        #while True:
-        #    scapy.send(arp_response,verbose=0)
+                self.MAC_ADDRESS_LIST.append(lines.split()[1])
+    def SAVE_LOG(self):      # Log kayıt eder
+        with open("logs.txt", "a") as file:
+            file.write(self.arpTable)
+            file.write("TARİH= " + str(datetime.now())+"\n")
+            file.write("SALDIRGAN IP: "+str(self.TARGET_IP_ADDRESS)+"\n")
+            file.write("SALDIRGAN MAC:"+str(self.TARGET_MAC_ADDRESS)+"\n")
+
+    def checkArpTable(self):
+        while True:
+            time.sleep(2)
+            if self.REAL_GATEWAY_MAC not in self.MAC_ADDRESS_LIST:                  ######----- SALDIRI YAPILDIĞINDA CALISAN YER -----#####
+                self.SAVE_LOG()
+                mixer.init()
+                mixer.music.load('dangerSound.mp3')
+                mixer.music.play()
+                os.system("netsh wlan disconnect")
+                self.zipIPandMAC()
+                self.setTargetMacAddress()
+                self.setAttackerIpAddress()
+                #self.ATTACK()
+            else:
+                self.MAC_ADDRESS_LIST.clear()
+                self.updateMacAddressList()
+
+    def setTargetMacAddress(self):
+        for lines in self.arpTable.splitlines():
+            if lines.split() == [] or "Interface:" in lines.split() or "Internet" in lines.split():
+                pass
+            else:
+                self.TARGET_MAC_ADDRESS=lines.split()[1]
+                break
+
+    def zipIPandMAC(self):
+        for lines in self.arpTable.splitlines():
+            if lines.split() == [] or "Interface:" in lines.split() or "Internet" in lines.split() or self.REAL_GATEWAY_MAC in lines.split():
+                pass
+            else:
+                self.IP_ADDRESS_LIST.append(lines.split()[0])
+                self.MAC_ADDRESS_LIST_FOR_DETECT_ATTACKER.append(lines.split()[1])
+        self.ZIP_IP_AND_MAC=dict(zip(self.MAC_ADDRESS_LIST_FOR_DETECT_ATTACKER,self.IP_ADDRESS_LIST))
+
+    def setAttackerIpAddress(self):
+        self.TARGET_IP_ADDRESS=self.ZIP_IP_AND_MAC.get(self.TARGET_MAC_ADDRESS)
 
 
-    else:
-        pass # sakin
+    #def ATTACK(self):
+    #    arp_response = scapy.ARP(op=2, pdst=self.TARGET_IP_ADDRESS, hwdst=self.TARGET_MAC_ADDRESS, psrc=self.REAL_GATEWAY_IP)
+    #    while True:
+    #        time.sleep(1)
+    #        scapy.send(arp_response, verbose=0)
+    #        print("**********PAKET GÖNERİLDİ**********")
 
-    yeniliste=dict(zip(ip_address,mac_address))
-    try:
-        yeniliste.pop("192.168.20.1")
-    except:
-        pass
-    dict_items = yeniliste.items()
-    for key, value in dict_items:
-        if value == modemMac:
-            target_ip=key
-            break
-
-
-
-
-
-
-
-
+obje=AntiMITM()
+obje.setRealGatewayMac()
+obje.updateMacAddressList()
+obje.checkArpTable()
 
 
