264c15a63d1bae5386035eff5267b3a83e873231
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-17 18:00:59 +0300

    Update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 from pygame import mixer
 from datetime import datetime
-import scapy.all as scapy
+
 class AntiMITM():
     def __init__(self):
         self.REAL_GATEWAY_MAC=None
@@ -14,52 +14,70 @@ class AntiMITM():
         self.TARGET_MAC_ADDRESS=None
         self.TARGET_IP_ADDRESS=None
         self.ZIP_IP_AND_MAC=None
+        self.CONNECT_STATUS=False
 
     def setRealGatewayMac(self):                             # tek bir sefer calısır ve orijinal modem mac adresini ayarlar
         with os.popen('arp -a') as f:
             arpTable = f.read()
         for lines in arpTable.splitlines():
-            if lines.split()==[] or "Interface:" in lines.split() or "Internet" in lines.split():
-                pass
-            else:
-                self.REAL_GATEWAY_MAC=lines.split()[1]
-                self.REAL_GATEWAY_IP=lines.split()[0]
+            if len(arpTable.splitlines())<10:
+                self.CONNECT_STATUS=False
                 break
+            else:
+                if lines.split()==[] or "Interface:" in lines.split() or "Internet" in lines.split():
+                    pass
+                else:
+                    self.REAL_GATEWAY_MAC=lines.split()[1]
+                    self.REAL_GATEWAY_IP=lines.split()[0]
+                    break
 
     def updateMacAddressList(self):
         with os.popen('arp -a') as f:                                      # Mac adreslerin bulunduğu listeyi günceller
             self.arpTable = f.read()
         for lines in self.arpTable.splitlines():
-            if lines.split() == [] or "Interface:" in lines.split() or "Internet" in lines.split():
-                pass
+            if len(self.arpTable.splitlines())<10:
+                self.CONNECT_STATUS=False
             else:
-                self.MAC_ADDRESS_LIST.append(lines.split()[1])
-    def SAVE_LOG(self):      # Log kayıt eder
-        with open("logs.txt", "a") as file:
-            file.write(self.arpTable)
-            file.write("TARİH= " + str(datetime.now())+"\n")
-            file.write("SALDIRGAN IP: "+str(self.TARGET_IP_ADDRESS)+"\n")
-            file.write("SALDIRGAN MAC:"+str(self.TARGET_MAC_ADDRESS)+"\n")
+                self.CONNECT_STATUS = True
+                if lines.split() == [] or "Interface:" in lines.split() or "Internet" in lines.split():
+                    pass
+                else:
+                    self.MAC_ADDRESS_LIST.append(lines.split()[1])
+
 
     def checkArpTable(self):
         while True:
             time.sleep(2)
-            if self.REAL_GATEWAY_MAC not in self.MAC_ADDRESS_LIST:                  ######----- SALDIRI YAPILDIĞINDA CALISAN YER -----#####
-                self.SAVE_LOG()
-                mixer.init()
-                mixer.music.load('dangerSound.mp3')
-                mixer.music.play()
-                #os.system("netsh wlan disconnect")
-                self.zipIPandMAC()
-                self.setTargetMacAddress()
-                self.setAttackerIpAddress()
+            if self.CONNECT_STATUS==False:
                 while True:
-                    time.sleep(1)
-                #self.ATTACK()
+                    #print("İNTERNET YOK")
+                    time.sleep(2)
+                    self.MAC_ADDRESS_LIST.clear()
+                    self.setRealGatewayMac()
+                    self.updateMacAddressList()
+                    if self.CONNECT_STATUS==True:
+                        time.sleep(1)
+                        #print("İNTERNETE BAGLANILDI")
+                        break
             else:
-                self.MAC_ADDRESS_LIST.clear()
-                self.updateMacAddressList()
-
+                #print("TARANIYOR")
+                #print(self.REAL_GATEWAY_MAC)
+                #print(self.REAL_GATEWAY_IP)
+                if self.REAL_GATEWAY_MAC not in self.MAC_ADDRESS_LIST:                  ######----- SALDIRI YAPILDIĞINDA CALISAN YER -----#####
+                    self.SAVE_LOG()
+                    mixer.init()
+                    mixer.music.load('dangerSound.mp3')
+                    mixer.music.play()
+                    os.system("netsh wlan disconnect")
+                    self.zipIPandMAC()
+                    self.setTargetMacAddress()
+                    self.setAttackerIpAddress()
+                    while True:
+                        time.sleep(1)
+                    #self.ATTACK()
+                else:
+                    self.MAC_ADDRESS_LIST.clear()
+                    self.updateMacAddressList()
 
     def setTargetMacAddress(self):
         for lines in self.arpTable.splitlines():
@@ -81,7 +99,12 @@ class AntiMITM():
     def setAttackerIpAddress(self):
         self.TARGET_IP_ADDRESS=self.ZIP_IP_AND_MAC.get(self.TARGET_MAC_ADDRESS)
 
-
+    def SAVE_LOG(self):      # Log kayıt eder
+        with open("logs.txt", "a") as file:
+            file.write(self.arpTable)
+            file.write("TARİH= " + str(datetime.now())+"\n")
+            file.write("SALDIRGAN IP: "+str(self.TARGET_IP_ADDRESS)+"\n")
+            file.write("SALDIRGAN MAC:"+str(self.TARGET_MAC_ADDRESS)+"\n")
     #def ATTACK(self):
     #    arp_response = scapy.ARP(op=2, pdst=self.TARGET_IP_ADDRESS, hwdst=self.TARGET_MAC_ADDRESS, psrc=self.REAL_GATEWAY_IP)
     #    while True:
